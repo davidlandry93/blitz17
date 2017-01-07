ac10148ef08b8bd40822edc290173dab54a4981d
David Landry <davidlandry93@gmail.com>
2017-01-07 08:43:21 -0500

    Added call to pathfinding

--- bot.py
+++ bot.py
@@ -1,5 +1,19 @@
 from random import choice
 from game import Game
+import requests
+from math import sqrt
+
+training_map = """################################C1    C2############F-            F-########  @1        @4  ######    []  B-B-  []    ####    ##  ####  ##    ####    ##  ####  ##    ####    []  B-B-  []    ######  @2        @3  ########F-            F-############C3    C4################################"""
+
+pathfinding_url = 'http://game.blitz.codes:8081/pathfinding/direction'
+def pathfinding(state, start, target):
+    payload = {'map': state, 'size': int(sqrt(len(training_map) / 2)), 'start': '(' + str(start[0]) + ',' + str(start[1]) + ')', 'target': '(' + str(target[0]) + ',' + str(target[1]) + ')'}
+    response = requests.get(pathfinding_url, params=payload)
+    return response.json()['direction']
+
+
+class PathfindingCall:
+    pass
 
 
 class Bot:
