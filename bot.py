1712344b3c32567e0dc05354a011a5b50328e029
mcarpentier <mathieuc6410@gmail.com>
2017-01-07 09:08:13 -0500

    Pathfinding

--- bot.py
+++ bot.py
@@ -21,14 +21,15 @@ class NullJsBot(Bot):
 
     def __init__(self):
         self.current_order = None  # {'burger': 1, 'fries': 2}
+        self.inventory = {'burger': 0, 'fries': 0}
 
     def move(self, state):
         game = Game(state)
         food_finder = FoodFinder(game)
-        hero_pos = state['hero']['pos']
+        hero_pos = (state['hero']['pos']['x'], state['hero']['pos']['y'])
 
         if not self.current_order:
-            self.current_order = {'burger': game.customers[0].burger, 'fries': game.customers[1].french_fries}
+            self.current_order = {'burger': game.customers[0].burger, 'fries': game.customers[0].french_fries}
 
         objective = None
         if self.current_order['burger'] > 1:
@@ -36,7 +37,6 @@ class NullJsBot(Bot):
         else:
             objective = food_finder.get_closest_fries(hero_pos)
 
-        dirs = ['Stay', 'North', 'South', 'East', 'West']
-        return choice(dirs)
+        return pathfinding(state['game']['board']['tiles'], hero_pos, objective)
 
 
