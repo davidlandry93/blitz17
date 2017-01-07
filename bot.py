2fd3207f1912a2e4c1bfb0bd4947d23ebc056a00
Jonathan Gingras <jonathan.gingras.1@gmail.com>
2017-01-07 13:16:47 -0500

    a star

--- bot.py
+++ bot.py
@@ -3,22 +3,28 @@ from game import Game
 from food import FoodFinder
 import requests
 from math import sqrt
+from path_finder import find_path, direction
 
 training_map = """################################C1    C2############F-            F-########  @1        @4  ######    []  B-B-  []    ####    ##  ####  ##    ####    ##  ####  ##    ####    []  B-B-  []    ######  @2        @3  ########F-            F-############C3    C4################################"""
 
 pathfinding_url = 'http://game.blitz.codes:8081/pathfinding/direction'
 
 # Here state is the string of the map.
-def pathfinding(state, start, target, size):
-    payload = {'map': state, 'size': size, 'start': '(' + str(start[0]) + ',' + str(start[1]) + ')', 'target': '(' + str(target[0]) + ',' + str(target[1]) + ')'}
-    response = requests.get(pathfinding_url, params=payload)
-    try:
-        direction = response.json()['direction']
-        return direction
-    except KeyError:
-        print('Random move')
-        return choice(['North', 'South', 'East', 'West'])
 
+#old version
+# def pathfinding(state, start, target, size):
+#     payload = {'map': state, 'size': size, 'start': '(' + str(start[0]) + ',' + str(start[1]) + ')', 'target': '(' + str(target[0]) + ',' + str(target[1]) + ')'}
+#     response = requests.get(pathfinding_url, params=payload)
+#     try:
+#         direction = response.json()['direction']
+#         return direction
+#     except KeyError:
+#         print('Random move')
+#         return choice(['North', 'South', 'East', 'West'])
+
+# AStar
+def pathfinding(state, start, target, size):
+    return direction(find_path(Board({'size': size, 'tiles': state}), start, target))
 
 
 
